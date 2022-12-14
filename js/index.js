12754d58700074661a10bfd217248a2e71b8ea20
+stephen.nene <+>
2022-12-14 10:48:30 +0300

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,7 @@
-print = (data)=>{
-    console.log(data)
+print = (value) => {
+    console.log(value)
 }
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+document.addEventListener("DOMContentLoaded",()=>{
+    print('mutua')
     
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
 })
---
+++ script.js
@@ -0,0 +1,21 @@
+print = (data)=>{
+    console.log(data)
+}
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // print(e.path)
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();print(location)
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+print(document.getElementById("login-form-submit"))
