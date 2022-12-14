ac955be4f00f081698508d28a1379c7c064207da
+stephen.nene <+>
2022-12-14 12:16:33 +0300

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-print = (value) => {
-    console.log(value)
-}
+print = (value) => {console.log(value)}
+const dummylogin = document.querySelector('main#main-holder')
+print(dummylogin)
 document.addEventListener("DOMContentLoaded",()=>{
-    print('mutua')
+    // print('mutua')
     
+    dummylogin.innerHTML =`
+    <h1 id="login-header">Login form</h1>
+    
+    <div id="login-error-msg-holder">
+      <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+    </div>
+
+    <form id="login-form">
+      <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
+      <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+      <input type="submit" value="Login" id="login-form-submit">
+    </form>
+    `
+
 })
--- script.js
+++ script.js
@@ -5,17 +5,16 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    // print(e.path)
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     // print(e.path)
+//     const username = loginForm.username.value;
+//     const password = loginForm.password.value;
     
-    // if (username === "user" && password === "web_dev") {
-    //     alert("You have successfully logged in.");
-    //     location.reload();print(location)
-    // } else {
-    //     loginErrorMsg.style.opacity = 1;
-    // }
-})
-print(document.getElementById("login-form-submit"))
+//     // if (username === "user" && password === "web_dev") {
+//     //     alert("You have successfully logged in.");
+//     //     location.reload();print(location)
+//     // } else {
+//     //     loginErrorMsg.style.opacity = 1;
+//     // }
+// })
