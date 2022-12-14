95330417e98baa51d8a1532568df82adf4c8fe69
+stephen.nene <+>
2022-12-14 12:34:26 +0300

    modified:   script.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 print = (value) => {console.log(value)}
-const dummylogin = document.querySelector('main#main-holder')
-print(dummylogin)
+
 document.addEventListener("DOMContentLoaded",()=>{
-    // print('mutua')
-    
-    dummylogin.innerHTML =`
+    document.body.innerHTML =`
+    <main id="main-holder">
     <h1 id="login-header">Login form</h1>
-    
     <div id="login-error-msg-holder">
       <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
     </div>
-
-    <form id="login-form">
+     <form id="login-form">
       <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
       <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
       <input type="submit" value="Login" id="login-form-submit">
     </form>
+  </main>
     `
+const submitBtn = document.querySelector('#login-form-submit')
+
+print(submitBtn)
+submitBtn.addEventListener('click',()=>{
 
 })
+
+})
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 print = (data)=>{
     console.log(data)
 }
-const loginForm = document.getElementById("login-form");
+addEventListener('DOMContentLoaded',()=>{
+    const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-// loginButton.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     // print(e.path)
-//     const username = loginForm.username.value;
-//     const password = loginForm.password.value;
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // print(e.path)
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
     
-//     // if (username === "user" && password === "web_dev") {
-//     //     alert("You have successfully logged in.");
-//     //     location.reload();print(location)
-//     // } else {
-//     //     loginErrorMsg.style.opacity = 1;
-//     // }
-// })
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();print(location)
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+})
