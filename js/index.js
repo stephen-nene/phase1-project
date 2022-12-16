c9d76a234c847e3f406ff43e19c86ce6dde87b23
+stephen.nene <+>
2022-12-16 07:58:11 +0300

    modified some html

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-
-   
 const submitBtn = document.querySelector('#login-form-submit')
 print(submitBtn)
-
-
+submitBtn.addEventListener('click', (event) => {
+event.preventDefault();
+})
 
 createMainContent=()=>{
     document.body.innerHTML = `
