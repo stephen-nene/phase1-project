e3166cb770668cf02cb3415b6aba2ed0fe76c73c
+stephen.nene <+>
2022-12-16 07:44:09 +0300

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,19 @@
 print = (value) => {console.log(value)}
 
 document.addEventListener("DOMContentLoaded",()=>{
-    document.body.innerHTML =`
-    <main id="main-holder">
-    <h1 id="login-header">Login form</h1>
-    <div id="login-error-msg-holder">
-      <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
-    </div>
-     <form id="login-form">
-      <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
-      <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
-      <input type="submit" value="Login" id="login-form-submit">
-    </form>
-  </main>
-    `
+   
 const submitBtn = document.querySelector('#login-form-submit')
-
 print(submitBtn)
 submitBtn.addEventListener('click',()=>{
+    createMainContent()   
+})})
 
-})
 
-})
-
+createMainContent=()=>{
+    document.body.innerHTML = `
+    `    
+    print('working')
+}
 
 
 
